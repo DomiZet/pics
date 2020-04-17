e758b51840f3db704afe182e9be86d4ceeab0a71
DomiZet <dominika.zwolenik@gmail.com>
2020-04-17 22:32:53 +0200

    added searching bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,23 +4,32 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+  state = { term:'' };
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  
+  onFormSubmit = event => {
+    event.preventDefault();
 
-  render () {
+    console.log(this.state.term);
+  };
+  
+
+  render() {
     
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
-          </div>
+            <input 
+            type="text"
+            value={this.state.term}
+            onChange={e => this.setState({ term: e.target.value})} 
+            />
+          </div> 
         </form>
       </div>
-    );
+    ); 
 
   }
 
@@ -28,4 +37,4 @@ class SearchBar extends React.Component {
 }
 
 
-export default SearchBar;
+export default SearchBar; 
